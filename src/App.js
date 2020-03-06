83b0753cb41579015507d6cd58b090088e9077bb
hanameee <hanameee@gmail.com>
2020-03-06 14:55:54 +0900

    Initiate directory and connect to backend application

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+
+function App() {
+    const [blogs, setBlogs] = useState();
+    const blog =
+        blogs &&
+        blogs.map(blog => (
+            <div
+                className="blogItem"
+                style={{ marginBottom: "10px" }}
+                key={blog._id}
+            >
+                <li>writer: {blog.author}</li>
+                <li>content: {blog.title}</li>
+            </div>
+        ));
+
+    useEffect(() => {
+        console.log("Useeffect");
+        axios.get("http://localhost:3001/api/blogs").then(response => {
+            setBlogs(response.data);
+            console.log(response.data);
+        });
+    }, []);
+
+    return (
+        <div className="App">
+            <h1>blog-list-frontend</h1>
+            <div className="blogList">
+                <ul>{blog}</ul>
+            </div>
+        </div>
+    );
+}
+
+export default App;
